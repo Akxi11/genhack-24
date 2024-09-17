a9ae004da6ad62736f75e232439b4ff06c24c5f5
Akxi11 <akshay.2023@vitstudent.ac.in>
2024-09-18 02:12:03 +0530

    Update timeline.tsx

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Timeline() {
     return (
-        <div className="w-full rounded-3xl flex flex-col items-center justify-center bg-fuchsia-400 border-white border-2">
+        {/* <div className="w-full rounded-3xl flex flex-col items-center justify-center bg-fuchsia-400 border-white border-2">
             timeline : AKSHAY
             <p>Use dotted timeline instead of just list</p>
             <p>
                 Try to use some scroll effects so that it comes into view as you
                 go down
             </p>
-            <p>Refer to timeline given on grp as discussed</p>
+            <p>Refer to timeline given on grp as discussed</p>*/}
         </div>
     );
 }
